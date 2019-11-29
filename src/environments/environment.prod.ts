fa7eaf6beeefee94238af5eb3de308fabca3b7fe
Paolangela la loca <pao93esp@gmail.com>
2019-11-29 20:23:25 -0300

    Production Firebase config added.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAE8_gHF-J2ySgvz2S6D6AE513S0CqFq1M",
+    authDomain: "investment-filling-tool.firebaseapp.com",
+    databaseURL: "https://investment-filling-tool.firebaseio.com",
+    projectId: "investment-filling-tool",
+    storageBucket: "investment-filling-tool.appspot.com",
+    messagingSenderId: "644574808703",
+    appId: "1:644574808703:web:b282220962d669693e4d4a"
+  }
 };
